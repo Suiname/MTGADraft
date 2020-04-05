5c5c5455a9d1b4488e0a69cab30ee29f1580144c
Jason Tham <jason@moonrisers.com>
2020-04-05 00:44:04 -0500

    Cube Cultivator Prototype - Created React App loading a mock collection from .json - Created Card element, loading images from scryfall - Created Cube List element, loading card names with removal buttons - Added Export button, exports all card names as .txt file in the appropriate format.

--- cubecultivator/src/App.js
+++ cubecultivator/src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const collection = require('./devData/collection.json');
+const Cards = require('./devData/MTGACards.json');
+
+const Card = ({active, index, onClick}) => {
+  return (
+    <div onClick={onClick} id={index} className={active ? "card active" : "card"}>
+      <img src={Cards[index].image_uris.en} alt={Cards[index].name}/>
+    </div>
+  );
+};
+
 function App() {
+  const [chosen, setChosen] = useState();
+  const [selected, setSelected] = useState([]);
+  const removeFromList = (id) => {
+    const filtered = selected.filter(selection => id !== selection);
+    setSelected(filtered);
+  };
+  const addToCube = () => !selected.includes(chosen) && setSelected([...selected, chosen]);
+  const xport = () => {
+    const cardList = selected.map((id) => Cards[id].name + '\n');
+    const data = new Blob(cardList, {type: 'text'});
+    const fileURL = window.URL.createObjectURL(data);
+    const tempLink = document.createElement('a');
+    tempLink.href = fileURL;
+    tempLink.setAttribute('download', 'filename.txt');
+    tempLink.click();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="cardContainerHeader"><h2>Group collection</h2></div>
+      <div id="cardContainer">
+        {
+          Object.keys(collection).map((key) => 
+            <Card active={key === chosen} index={key} onClick={() => setChosen(key)} key={key} />
+          )
+        }
+      </div>
+      <div id="selectorContainer">
+        <button onClick={addToCube}>Add to Cube</button><button onClick={xport}>Export Cube</button>
+      </div>
+      <div id="listContainerHeader"><h2>Cube Card List</h2></div>
+      <div id="listContainer">
+      <div id="statContainer">Card Count: {selected.length}</div>
+        {
+          selected && selected.map((id) => 
+          <div className="listItem" key={id}>
+            <span>{Cards[id].name}</span>
+            <button onClick={() => removeFromList(id)}>Remove</button>
+          </div>
+          )
+        }
+      </div>
     </div>
   );
 }
