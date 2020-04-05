d4ac58158eb42747d142dbbae61981916ee4d6ea
Jason Tham <jason@moonrisers.com>
2020-04-04 22:55:22 -0500

    Create React App - Created scaffolding for react app in `/cubecultivator` - Created symlink to build directory in `/public/cube` - Updated package.json to serve react app at `http://baseurl/cube

--- server.js
+++ server.js
@@ -21,33 +21,42 @@ const SessionModule = require("./Session");
 const Session = SessionModule.Session;
 const Sessions = SessionModule.Sessions;
 
-const privateKey = fs.readFileSync('./certs/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('./certs/cert.pem', 'utf8');
-const cs = fs.readFileSync('./certs/chain.pem', 'utf8');
+let https, io;
+if (process.env.NODE_ENV == 'production') { // Load certs in production, run sockets over SSL
+	const privateKey = fs.readFileSync('./certs/privkey.pem', 'utf8');
+	const certificate = fs.readFileSync('./certs/cert.pem', 'utf8');
+	const cs = fs.readFileSync('./certs/chain.pem', 'utf8');
 
-const credentials = {
-	key: privateKey,
-	cert: certificate,
-	ca: cs
-};
+	const credentials = {
+		key: privateKey,
+		cert: certificate,
+		ca: cs
+	};
 
-const https = require('https').Server(credentials, app);
-const io = require('socket.io')(https);
+	https = require('https').Server(credentials, app);
+	io = require('socket.io')(https);
+} else {
+	io = require('socket.io')(http);
+}
+
 
 const router = require('./routes'); 
 
 app.use(compression());
 app.use(cookieParser());
 
-app.use (function (req, res, next) {
-	if (req.secure) {
-			// request was via https, so do no special handling
-			next();
-	} else {
-			// request was via http, so redirect to https
-			res.redirect('https://' + req.headers.host + req.url);
-	}
-});
+if (process.env.NODE_ENV == 'production') {  // Force SSL if in production
+	app.use (function (req, res, next) {
+		if (req.secure) {
+				// request was via https, so do no special handling
+				next();
+		} else {
+				// request was via http, so redirect to https
+				res.redirect('https://' + req.headers.host + req.url);
+		}
+	});
+}
+
 
 function shortguid() {
 	function s4() {
@@ -751,21 +760,24 @@ app.use((req, _res, next) => {
 
 app.use('/api', router);
 
-Promise.all([Persistence.InactiveConnections, Persistence.InactiveSessions]).then((values) => {
-	InactiveConnections = values[0];
-	InactiveSessions = values[1];
-	http.listen(port, (err) => {
-		if (err) throw err;
-		console.log("listening on port " + port);
-	});
-
-	https.listen(443, (err) => {
-        if(err)
-               throw err;
-        console.log('listening securely on port 443');
-	});
-});
 
 app.get('*', function(req, res) {  
     res.redirect('https://' + req.headers.host + req.url);
 })
+
+Promise.all([Persistence.InactiveConnections, Persistence.InactiveSessions]).then((values) => {
+	InactiveConnections = values[0];
+	InactiveSessions = values[1];
+	http.listen(port, (err) => { 
+		if(err) 
+			throw err; 
+		console.log('listening on port ' + port); 
+	});
+	if (process.env.NODE_ENV == 'production') {
+		https.listen(443, (err) => {
+			if(err)
+					throw err;
+			console.log('listening securely on port 443');
+		});
+	}
+});
