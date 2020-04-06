940f8ac02618a8bd20b9640741d3d7374ce2cf5c
Jason Tham <jason@moonrisers.com>
2020-04-06 02:54:03 -0500

    Cube Cultivator UI Improvements - Added pagination, page navigation and display - Added name filter, filtering collection by name - Add color filter checkboxes, filtering collection by color - Added rarity filter checkboxes, filtering collection by rarity - Improved add / remove card buttons, moved to on top of card image - Added basic stats about the collection

---
+++ cubecultivator/build/precache-manifest.c96e029a1bef837dd15b5bc8624c1734.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a2df0ffde75b5b611cd04ec4030caca",
+    "url": "/cube/index.html"
+  },
+  {
+    "revision": "cce774ee347c9d6c1317",
+    "url": "/cube/static/css/main.738aa1e2.chunk.css"
+  },
+  {
+    "revision": "c14ad04efd45da174873",
+    "url": "/cube/static/js/2.addfa335.chunk.js"
+  },
+  {
+    "revision": "f45b16f131ddcdb87372e3f8c0d06dbd",
+    "url": "/cube/static/js/2.addfa335.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cce774ee347c9d6c1317",
+    "url": "/cube/static/js/main.71376e7d.chunk.js"
+  },
+  {
+    "revision": "824fa8bbcdc73b24211a",
+    "url": "/cube/static/js/runtime-main.fe36f40e.js"
+  }
+]);
