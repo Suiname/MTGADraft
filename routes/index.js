8fe6b218969d4f72afa522c18f8787a7491011a2
Jason Tham <jason@moonrisers.com>
2020-04-07 11:32:49 -0500

    Use API to Load Card Data - Created /api/cards/all endpoint to serve up the Cards JSON - Created loading states, loading spinner while data is loading

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const collection = require('./collection');
+const cards = require('./cards');
 
 const express = require('express');
 const router = express.Router();
 
 // define the home page route
 router.use('/collection', collection);
+router.use('/cards', cards);
 
 module.exports = router;
